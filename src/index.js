abcdfa36f59f51cb50fa34a46550e6f32a608419
Tigran <tigran2589@gmail.com>
2022-02-17 08:35:44 +0200

    Add transformCharacter method

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
+//import MarvelService from './services/MarvelService';
+
 import './style/style.scss';
+
+
+
+//marvelService.getAllCharacters().then(res => console.log(res.data.results.forEach(item => console.log(item.name))));
 
 ReactDOM.render(
   <React.StrictMode>
